f97b0927f70004de5e93e98fe53af832d6a4b8e1
TwilightStar <tugracimenxtablet@gmail.com>
2026-09-13 02:42:24 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,17 @@ import 'package:flutter/services.dart';
 import 'core/theme/app_colors.dart';
 import 'core/theme/app_theme.dart';
 import 'screens/home_screen.dart';
+import 'services/settings_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // AMOLED cihazlarda durum/gezinme cubuklarinin da gercek siyah
-  // olmasini sagliyoruz; Godot tarafindaki `boot_splash/bg_color`
-  // ayarinin Flutter karsiligi.
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: AppColors.background,
-      systemNavigationBarIconBrightness: Brightness.light,
-    ),
-  );
+  // Uygulama açılışında kayıtlı temayı yükle; hiç seçilmediyse
+  // varsayılan AMOLED tema kullanılır (bkz. `AppThemeCodec`).
+  final savedThemeKey = await SettingsService().getThemeName();
+  AppColors.setTheme(AppThemeCodec.fromStorageKey(savedThemeKey));
+
+  _applySystemChrome();
 
   // Godot projesindeki `window/handheld/orientation="portrait"` ayarinin
   // karsiligi.
@@ -30,8 +26,49 @@ void main() {
   runApp(const TerminalApp());
 }
 
-class TerminalApp extends StatelessWidget {
+/// AMOLED cihazlarda durum/gezinme cubuklarinin da secili temanin zemin
+/// rengiyle uyumlu olmasini sagliyoruz; Godot tarafindaki
+/// `boot_splash/bg_color` ayarinin Flutter karsiligi. Tema her
+/// degistiginde (bkz. `_TerminalAppState`) yeniden cagrilir ki
+/// gezinme cubugu rengi de aninda guncellensin.
+void _applySystemChrome() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: AppColors.background,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+}
+
+class TerminalApp extends StatefulWidget {
   const TerminalApp({super.key});
+
+  @override
+  State<TerminalApp> createState() => _TerminalAppState();
+}
+
+class _TerminalAppState extends State<TerminalApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Tema, uygulama içinden (Kontroller sekmesi) değiştirildiğinde
+    // `AppColors.notifier` tetiklenir - burada dinleyip hem `ThemeData`
+    // hem de sistem çubuğu rengini yeniden kuruyoruz.
+    AppColors.notifier.addListener(_onThemeChanged);
+  }
+
+  @override
+  void dispose() {
+    AppColors.notifier.removeListener(_onThemeChanged);
+    super.dispose();
+  }
+
+  void _onThemeChanged() {
+    _applySystemChrome();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
